e839aa4e4839a219b8d004c1040b9ae9a901c91c
Linkon Miyan <rudlinkon@gmail.com>
2023-08-18 13:02:30 +0600

    added router example

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,39 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import * as React from "react";
+import * as ReactDOM from "react-dom/client";
+import Root, {loader as rootLoader, action as rootAction} from "./routes/Root.jsx";
+import Contact, {loader as contactLoader} from "./routes/Contact.jsx";
+import ErrorPage from "./ErrorPage.jsx";
+import {
+    createBrowserRouter,
+    RouterProvider,
+} from "react-router-dom";
+import "./index.css";
+import EditContact from "./routes/EditContact";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+const router = createBrowserRouter([
+    {
+        path: "/testrepo/",
+        element: <Root />,
+        errorElement: <ErrorPage />,
+        loader: rootLoader,
+        action: rootAction,
+        children: [
+            {
+                path: "contacts/:contactId",
+                element: <Contact />,
+                loader: contactLoader
+            },
+            {
+                path: "contacts/:contactId/edit",
+                element: <EditContact />,
+                loader: contactLoader,
+            },
+        ]
+    }
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+    <React.StrictMode>
+        <RouterProvider router={router} />
+    </React.StrictMode>
+);
